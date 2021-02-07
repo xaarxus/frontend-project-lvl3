bca0473bafec5143aa2d4a2f0a99bf562afcbfb9
newsalaar <keyshet@gmail.com>
2021-02-07 13:43:57 +0100

    add webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  entry: {
+    main: './src/index.js',
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
