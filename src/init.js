97a47b67c827ae6bbb61d756ba6dbee8e04b76d9
newsalaar <keyshet@gmail.com>
2021-02-12 15:55:16 +0100

    add parser & add dep (on-change)

--- src/init.js
+++ src/init.js
@@ -31,7 +31,7 @@ export default () => {
           const { value, rssFlows } = state;
           if (rssFlows.includes(value)) {
             input.setAttribute('class', 'border border-danger form-control form-control-lg w-100');
-            feedback.innerHTML = '<p class="text-success text-danger">rss flow has already been added</p>';
+            feedback.innerHTML = '<p class="text-success text-danger">Rss already exists</p>';
             return;
           }
           state.rssFlows = [...rssFlows, value];
@@ -39,7 +39,17 @@ export default () => {
           input.value = '';
           feedback.innerHTML = '<p class="text-success">RSS has been loaded</p>';
           axios.get(value)
-            .then((respose) => console.log(parser(respose.data)));
+            .then((respose) => {
+              const data = parser(respose.data);
+              const { newFeed, newPosts } = data;
+
+              const { feeds, posts } = state;
+
+              state.feeds = [newFeed, ...feeds];
+              state.posts = [newPosts, ...posts];
+
+              console.log(newFeed, newPosts);
+            });
         } else {
           input.setAttribute('class', 'border border-danger form-control form-control-lg w-100');
           feedback.innerHTML = '<p class="text-success text-danger">It\'s not valid RSS link</p>';
--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,20 @@
 export default (data) => {
   const parser = new DOMParser();
-  return parser.parseFromString(data, 'text/xml');
+  const xml = parser.parseFromString(data, 'text/xml');
+  const channel = xml.querySelector('channel');
+
+  const feedTitle = channel.querySelector('title').innerHTML;
+  const feedDescription = channel.querySelector('description').innerHTML;
+  const newFeed = { title: feedTitle, description: feedDescription };
+
+  const posts = channel.querySelectorAll('item');
+  const newPosts = [];
+  posts.forEach((item) => {
+    const title = item.querySelector('title').innerHTML;
+    const description = item.querySelector('description').innerHTML;
+    const link = item.querySelector('link').innerHTML;
+    newPosts.push({ title, description, link });
+  });
+
+  return { newFeed, newPosts };
 };
