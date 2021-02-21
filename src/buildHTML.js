3df900078da61f3e8095e100546f9b6e4fb1fbcc
newsalaar <keyshet@gmail.com>
2021-02-21 16:14:43 +0100

    remove buildHTML

--- src/buildHTML.js
+++
@@ -1,9 +0,0 @@
-export default (i18next) => {
-  document.querySelector('h1').innerHTML = i18next.t('texts.h1');
-  document.querySelector('.example').innerHTML = i18next.t('texts.example');
-  document.querySelector('#add').innerHTML = i18next.t('texts.form.add');
-  document.querySelector('#formInput').setAttribute('placeholder', i18next.t('texts.form.input'));
-
-  document.querySelector('.full-article').innerHTML = i18next.t('texts.modal.fullArticle');
-  document.querySelector('#close').innerHTML = i18next.t('texts.modal.close');
-};
--- src/init.js
+++ src/init.js
@@ -1,9 +1,15 @@
-import buildHTML from './buildHTML.js';
 import watchedState from './appModal.js';
 import { formValidator } from './appController.js';
 
 export default () => {
-  buildHTML(watchedState.i18next);
+  const { i18next } = watchedState;
+  document.querySelector('h1').innerHTML = i18next.t('texts.h1');
+  document.querySelector('.example').innerHTML = i18next.t('texts.example');
+  document.querySelector('#add').innerHTML = i18next.t('texts.form.add');
+  document.querySelector('#formInput').setAttribute('placeholder', i18next.t('texts.form.input'));
+
+  document.querySelector('.full-article').innerHTML = i18next.t('texts.modal.fullArticle');
+  document.querySelector('#close').innerHTML = i18next.t('texts.modal.close');
 
   const form = document.querySelector('form');
   form.addEventListener('submit', formValidator);
